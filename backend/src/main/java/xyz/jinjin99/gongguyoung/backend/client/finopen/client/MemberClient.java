919d0d4eeff179797148f3f68f74823976b69dcb
bbangho <dudgh614@gmail.com>
2025-08-22 17:41:19 +0900

    feature/signup-member-with-account

--- backend/src/main/java/xyz/jinjin99/gongguyoung/backend/client/finopen/client/MemberClient.java
+++ backend/src/main/java/xyz/jinjin99/gongguyoung/backend/client/finopen/client/MemberClient.java
@@ -6,4 +6,6 @@ import xyz.jinjin99.gongguyoung.backend.client.finopen.dto.common.MemberRecord;
 
 public interface MemberClient {
   MemberRecord getOrCreateMember(@NotBlank @Email String userId);
+  MemberRecord createMember(@NotBlank @Email String userId) ;
+  MemberRecord searchMember(@NotBlank @Email String userId) ;
 }
